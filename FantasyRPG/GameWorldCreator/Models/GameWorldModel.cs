c93cc3a69f0b89225441bf985d39db20485767b5
naStipe <stepan.khlebushkin@gmail.com>
2024-10-15 22:17:10 +0300

    Added world time enum

---
+++ FantasyRPG/GameWorldCreator/Models/GameWorldModel.cs
@@ -0,0 +1,12 @@
+namespace FantasyRPG.GameWorldCreator.Models;
+
+public class GameWorld
+{
+    public enum WorldTimeEnum
+    {
+        Morning,
+        Day,
+        Afternoon,
+        Night
+    }
+}
