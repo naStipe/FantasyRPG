2814d5cd8e407f02da32bd3e4a2bc23abcccdf8f
naStipe <stepan.khlebushkin@gmail.com>
2024-10-15 22:20:18 +0300

    Created class WorldMap with tiles and biomes

--- FantasyRPG/GameWorldCreator/Models/GameWorldModel.cs
+++ FantasyRPG/GameWorldCreator/Models/GameWorldModel.cs
@@ -2,6 +2,46 @@ namespace FantasyRPG.GameWorldCreator.Models;
 
 public class GameWorld
 {
+    
+    public class WorldMap
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public Tile[,] Map { get; private set; }
+
+        public WorldMap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Map = new Tile[width, height];
+        }
+    }
+    
+    public class Tile
+    {
+        public string Biome { get; set; }
+        public int Elevation { get; set; }
+
+        public Tile(string biome, int elevation)
+        {
+            Biome = biome;
+            Elevation = elevation;
+        }
+    }
+    
+    public class Biome
+    {
+        public string Name { get; set; }
+        public int MinElevation { get; set; }
+        public int MaxElevation { get; set; }
+
+        public Biome(string name, int minElevation, int maxElevation)
+        {
+            Name = name;
+            MinElevation = minElevation;
+            MaxElevation = maxElevation;
+        }
+    }
     public enum WorldTimeEnum
     {
         Morning,
