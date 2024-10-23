b5adc822669d3080b0505f8f1713017992db18cd
naStipe <stepan.khlebushkin@gmail.com>
2024-10-23 14:28:58 +0300

    Created classes for different character states (IdleState, ActionState, DefendingState)

--- FantasyRPG/CharacterCreator/Models/States.cs
+++ FantasyRPG/CharacterCreator/Models/States.cs
@@ -5,3 +5,34 @@ public interface ICharacterState
     void HandleState(string characterName);
 }
 
+public class IdleState : ICharacterState
+{
+    public void HandleState(string characterName)
+    {
+        Console.WriteLine($"{characterName} is in idle state.");
+    }
+}
+
+public class ActionState : ICharacterState
+{
+    private IActionStrategy _actionStrategy;
+
+    public ActionState(IActionStrategy actionStrategy)
+    {
+        _actionStrategy = actionStrategy;
+    }
+
+    public void HandleState(string characterName)
+    {
+        Console.WriteLine($"{characterName} is in action mode.");
+        _actionStrategy.PerformAction(characterName);
+    }
+}
+
+public class DefendingState : ICharacterState
+{
+    public void HandleState(string characterName)
+    {
+        Console.WriteLine($"{characterName} is defending.");
+    }
+}
