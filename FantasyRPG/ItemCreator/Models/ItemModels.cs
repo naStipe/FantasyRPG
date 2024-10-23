69f254c92b8f72b1c42356b5dfd882adef0b0fcd
naStipe <stepan.khlebushkin@gmail.com>
2024-10-23 14:13:03 +0300

    Created constructor for the item class

--- FantasyRPG/ItemCreator/Models/ItemModels.cs
+++ FantasyRPG/ItemCreator/Models/ItemModels.cs
@@ -8,8 +8,17 @@ public abstract class Item
     public string Name { get; set; }
     public string Description { get; set; }
     public RarityEnum Rarity { get; set; }
-    
-    
+
+
+    public Item(string name, string description, RarityEnum rarity)
+    {
+        Id = nextId++;
+        Name = name;
+        Description = description;
+        Rarity = rarity;
+    }
+
+    public abstract void DisplayInfo();
 }
 
 public enum RarityEnum
