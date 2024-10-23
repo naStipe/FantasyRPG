c79998c8026a4bbdf2fc0eb860c3522b6d76d934
naStipe <stepan.khlebushkin@gmail.com>
2024-10-23 14:27:11 +0300

    Created classes for different strategies (DefaultAction, AttackAction, SpellAction, and ItemAction)

--- FantasyRPG/CharacterCreator/Models/Strategies.cs
+++ FantasyRPG/CharacterCreator/Models/Strategies.cs
@@ -4,3 +4,35 @@ public interface IActionStrategy
 {
     void PerformAction(string characterName);
 }
+
+public class DefaultAction : IActionStrategy
+{
+    public void PerformAction(string characterName)
+    {
+        Console.WriteLine($"{characterName} is looking around curiously");
+    }
+}
+
+public class AttackAction : IActionStrategy
+{
+    public void PerformAction(string characterName)
+    {
+        Console.WriteLine($"{characterName} is perfroming an attack!");
+    }
+}
+
+public class SpellAction : IActionStrategy
+{
+    public void PerformAction(string characterName)
+    {
+        Console.WriteLine($"{characterName} is casting a magic spell!");
+    }
+}
+
+public class ItemAction : IActionStrategy
+{
+    public void PerformAction(string characterName)
+    {
+        Console.WriteLine($"{characterName} is performing melee attack!");
+    }
+}
