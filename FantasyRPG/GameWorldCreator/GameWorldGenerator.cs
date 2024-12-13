3827c82e6f366888e4fc5cc48189f0c75cac025b
naStipe <stepan.khlebushkin@gmail.com>
2024-12-13 16:51:49 +0200

    Changes to gameworld model and generator

--- FantasyRPG/GameWorldCreator/GameWorldGenerator.cs
+++ FantasyRPG/GameWorldCreator/GameWorldGenerator.cs
@@ -1,11 +1,12 @@
-using FantasyRPG.GameWorldCreator.Models;
+﻿using DesignPatternsFantasyRPG.GameWorldCreator.Models;
 
-namespace FantasyRPG.GameWorldCreator;
-
-public class GameWorldGenerator
+namespace DesignPatternsFantasyRPG.GameWorldCreator
+{
+    public class GameWorldGenerator
     {
         private static Random random = new Random();
 
+        // Generate new game world map
         public static WorldMap GenerateWorldMap(int width, int height, List<Biome> biomes)
         {
             WorldMap worldMap = new WorldMap(width, height);
@@ -14,10 +15,13 @@ public class GameWorldGenerator
             {
                 for (int y = 0; y < height; y++)
                 {
+                    // Generate elevation randomly
                     int elevation = random.Next(0, 200);
 
+                    // Determine biome based on elevation
                     Biome selectedBiome = SelectBiome(biomes, elevation);
 
+                    // Create the tile with selected biome and attributes
                     worldMap.Map[x, y] = new Tile(selectedBiome.Name, elevation);
                 }
             }
@@ -25,6 +29,7 @@ public class GameWorldGenerator
             return worldMap;
         }
 
+        // TODO: Could enhance Biome generation -> e.g. certain biomes can't be connected to each other
         private static Biome SelectBiome(List<Biome> biomes, int elevation)
         {
             foreach (var biome in biomes)
@@ -35,53 +40,58 @@ public class GameWorldGenerator
                 }
             }
 
+            // Default biome if no match (you could also throw an exception)
             return biomes[0];
         }
 
+        // Terminal colors for biomes 
+        // TODO: Could move to contructor inside Biome class instead
         private static void SetBiomeColor(string biome)
         {
             switch (biome)
             {
                 case "Lake":
-                    Console.ForegroundColor = ConsoleColor.Blue;  
+                    Console.ForegroundColor = ConsoleColor.Blue;  // Blue for Lake
                     break;
                 case "Forest":
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;  
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;  // DarkGreen for Forest
                     break;
                 case "Desert":
-                    Console.ForegroundColor = ConsoleColor.Yellow;  
+                    Console.ForegroundColor = ConsoleColor.Yellow;  // Yellow for Desert
                     break;
                 case "Mountain":
-                    Console.ForegroundColor = ConsoleColor.Gray;  
+                    Console.ForegroundColor = ConsoleColor.Gray;  // Gray for Mountain
                     break;
                 case "Plains":
-                    Console.ForegroundColor = ConsoleColor.Green;  
+                    Console.ForegroundColor = ConsoleColor.Green;  // Green for Plains
                     break;
                 default:
-                    Console.ForegroundColor = ConsoleColor.White;  
+                    Console.ForegroundColor = ConsoleColor.White;  // Default to white
                     break;
             }
         }
 
-        
+        // Reset console color to default
         private static void ResetColor()
         {
             Console.ResetColor();
         }
 
-        
+        // Print currently generated map
         public static void PrintMap(WorldMap worldMap)
         {
+            // TODO: Could use more sophisticated approach
             for (int y = 0; y < worldMap.Height; y++)
             {
                 for (int x = 0; x < worldMap.Width; x++)
                 {
                     var tile = worldMap.Map[x, y];
                     SetBiomeColor(tile.Biome);
-                    Console.Write(tile.Biome[0] + " ");  
+                    Console.Write(tile.Biome[0] + " ");  // Print the first letter of the biome
                     ResetColor();
                 }
-                Console.WriteLine();  
+                Console.WriteLine();  // New line after each row
             }
         }
     }
+}
--- FantasyRPG/GameWorldCreator/Models/GameWorldModel.cs
+++ FantasyRPG/GameWorldCreator/Models/GameWorldModel.cs
@@ -1,94 +1,106 @@
-using FantasyRPG.CharacterCreator.Models;
+﻿using DesignPatternsFantasyRPG.CharacterCreator.Models;
 
-namespace FantasyRPG.GameWorldCreator.Models;
-
-public class GameWorld
+namespace DesignPatternsFantasyRPG.GameWorldCreator.Models
 {
-    
-    private static GameWorld _instance;
-
-    private static readonly object lockObj = new object();
-
-    public WorldMap WorldMap { get; private set; }
-    public List<Character> WorldCharacters { get; private set; }
-    public WorldTimeEnum TimeOfDay { get; set; }
-
-    private GameWorld()
+    public class GameWorld
     {
-        WorldCharacters = new List<Character>();
-        TimeOfDay = WorldTimeEnum.Morning; 
-    }
+        // Static variable that holds the single instance
+        private static GameWorld _instance;
 
-    public static GameWorld Instance
-    {
-        get
+        // Object used for locking to avoid threading issues
+        private static readonly object lockObj = new object();
+
+        public WorldMap WorldMap { get; private set; }
+        public List<Character> WorldCharacters { get; private set; }
+        public WorldTimeEnum TimeOfDay { get; set; }
+
+        // Private constructor to prevent external instantiation
+        private GameWorld()
         {
-            lock (lockObj)
+            WorldCharacters = new List<Character>();
+            TimeOfDay = WorldTimeEnum.Morning; // Default value
+        }
+
+        // Public static method to access the single instance
+        public static GameWorld Instance
+        {
+            get
             {
-                if (_instance == null)
+                lock (lockObj)
                 {
-                    _instance = new GameWorld();
+                    if (_instance == null)
+                    {
+                        _instance = new GameWorld();
+                    }
+                    return _instance;
                 }
-                return _instance;
             }
         }
+
+        // Initialize the world
+        public void InitializeWorld(int width, int height, List<Biome> biomes)
+        {
+            WorldMap = GameWorldGenerator.GenerateWorldMap(width, height, biomes);
+        }
+
+        // Add an character to the world
+        public void AddCharacter(Character character)
+        {
+            WorldCharacters.Add(character);
+        }
     }
-    
-    public void InitializeWorld(int width, int height, List<Biome> biomes)
+
+    // Represents a single tile on the map, which will have properties like biome, elevation, etc.
+    public class Tile
     {
-        WorldMap = GameWorldGenerator.GenerateWorldMap(width, height, biomes);
+        public string Biome { get; set; }
+        public int Elevation { get; set; }
+
+        // TODO: Add support for locations (towns, dungeons) -> events??
+
+        public Tile(string biome, int elevation)
+        {
+            Biome = biome;
+            Elevation = elevation;
+        }
     }
-    
-    public void AddCharacter(Character character)
+
+    // Represents different biomes with their own characteristics.
+    public class Biome
     {
-        WorldCharacters.Add(character);
+        public string Name { get; set; }
+        public int MinElevation { get; set; }
+        public int MaxElevation { get; set; }
+
+        public Biome(string name, int minElevation, int maxElevation)
+        {
+            Name = name;
+            MinElevation = minElevation;
+            MaxElevation = maxElevation;
+        }
+    }
+
+    // Represents the entire game world, which is a grid of tiles.
+    public class WorldMap
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public Tile[,] Map { get; private set; }
+
+        public WorldMap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Map = new Tile[width, height];
+        }
+    }
+
+    // Enumerable for world time
+    public enum WorldTimeEnum
+    {
+        Morning,
+        Day,
+        Afternoon,
+        Night
     }
 }
-    
-public class WorldMap
-{
-    public int Width { get; private set; }
-    public int Height { get; private set; }
-    public Tile[,] Map { get; private set; }
-
-    public WorldMap(int width, int height)
-    {
-        Width = width;
-        Height = height;
-        Map = new Tile[width, height];
-    }
-}
-
-public class Tile
-{
-    public string Biome { get; set; }
-    public int Elevation { get; set; }
-
-    public Tile(string biome, int elevation)
-    {
-        Biome = biome;
-        Elevation = elevation;
-    }
-}
-
-public class Biome
-{
-    public string Name { get; set; }
-    public int MinElevation { get; set; }
-    public int MaxElevation { get; set; }
-
-    public Biome(string name, int minElevation, int maxElevation)
-    {
-        Name = name;
-        MinElevation = minElevation;
-        MaxElevation = maxElevation;
-    }
-}
-public enum WorldTimeEnum
-{
-    Morning,
-    Day,
-    Afternoon,
-    Night
-}
-
